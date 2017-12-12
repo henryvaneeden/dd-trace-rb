bed24b68da268616d5083d5b024f630c18080e5d
Henry van Eeden <henry@swipeinteractive.co.za>
2017-12-12 12:30:29 +0200

    remove logs

--- lib/ddtrace/contrib/rails/action_view.rb
+++ lib/ddtrace/contrib/rails/action_view.rb
@@ -12,7 +12,6 @@ module Datadog
 
         def self.start_render_template(payload)
           # retrieve the tracing context
-            Datadog::Tracer.log.debug("start_render_template zone 1")
           tracing_context = payload.fetch(:tracing_context)
 
           # create a new Span and add it to the tracing context
@@ -24,7 +23,6 @@ module Datadog
         end
 
         def self.finish_render_template(payload)
-          Datadog::Tracer.log.debug("finish_render_template zone 1")
           # retrieve the tracing context and the latest active span
           tracing_context = payload.fetch(:tracing_context)
           span = tracing_context[:dd_rails_template_span]
@@ -47,7 +45,6 @@ module Datadog
         end
 
         def self.start_render_partial(payload)
-          Datadog::Tracer.log.debug("start_render_partial zone 2")
           # retrieve the tracing context
           tracing_context = payload.fetch(:tracing_context)
 
@@ -59,8 +56,6 @@ module Datadog
         end
 
         def self.finish_render_partial(payload)
-            Datadog::Tracer.log.debug("finish_render_partial zone 3")
-            Datadog::Tracer.log.debug("payload #{payload.to_yaml}")
 
           # retrieve the tracing context and the latest active span
           tracing_context = payload.fetch(:tracing_context)
